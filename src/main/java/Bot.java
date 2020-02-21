a0ab635176e8eaf013fcd7a05fb7ffe75cc71127
Marcel Musteata <Marcel.Musteata@endava.com>
2020-02-21 12:41:11 +0200

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,58 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.logging.*;
+
+import static org.telegram.telegrambots.logging.BotLogger.log;
+
+public class Bot extends TelegramLongPollingBot {
+
+
+    /**
+     * Method for receiving messages.
+     * @param update contain message from user.
+     */
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Method return bot username, which was set at registration.
+     * @return bot username
+     */
+
+    @Override
+    public String getBotUsername() {
+        return "MyHelperNotificationbot";
+    }
+
+    /**
+     * Method return bot's token for communication with Telegram server
+     * @return token for bot
+     */
+
+    @Override
+    public String getBotToken() {
+        return "933877450:AAELtutyrU9uDgQP0ETldF3ybCQ52I3-RXQ";
+    }
+
+}
